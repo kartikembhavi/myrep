fdaf29128a38b5dda9858780887e1bb35f0dd656
kartikembhavi <64646492+kartikembhavi@users.noreply.github.com>
2020-05-09 19:40:59 +0530

    Update AccountListHelper.js

--- Lightning Component/AccountListHelper.js
+++ Lightning Component/AccountListHelper.js
@@ -2,7 +2,9 @@
       // Fetch the accounts from the Apex controller
       getAccountList: function(component) {
         var action = component.get('c.getAccounts');
-        action.setCallback(this, function(actionResult) {
+        // Set up the callback
+        var self = this;
+        action.setCallback(self, function(actionResult) {
          component.set('v.accounts', actionResult.getReturnValue());
         });
         $A.enqueueAction(action);
